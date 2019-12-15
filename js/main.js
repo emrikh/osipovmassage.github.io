15b799723885fb44f43ab5fce2fae6ec51ea0b15
nikolayemrikh <nikolayemrikh@htmlacademy.ru>
2019-12-16 02:43:29 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-!function(){"use strict";var n=document.querySelector(".main-nav");n.querySelector(".main-nav__toggle").addEventListener("click",function(){n.classList.toggle("main-nav--shown")})}();
+!function(){"use strict";var e=document.querySelector(".main-nav");e.querySelector(".main-nav__toggle").addEventListener("click",function(){e.classList.toggle("main-nav--shown")}),document.querySelectorAll(".services__list-item-btn").forEach(function(e){e.addEventListener("click",function(t){t.preventDefault(),e.closest(".services__list-item").classList.toggle("services__list-item--active")})})}();
 //# sourceMappingURL=main.js.map
